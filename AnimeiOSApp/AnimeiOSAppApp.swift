efcfdbdb9ee5da718d4eb7a756bad28f56441192
fummicc1 <fumiya.tennis1@gmail.com>
2022-06-24 22:17:49 +0900

    Initial Commit

---
+++ AnimeiOSApp/AnimeiOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeiOSAppApp.swift
+//  AnimeiOSApp
+//
+//  Created by Fumiya Tanaka on 2022/06/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimeiOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
